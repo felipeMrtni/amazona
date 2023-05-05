92dd21c7d5c41bfc138fb7c621d9f44c9ea3d340
Felipe Martini <felipecmartini@gmail.com>
2023-05-04 21:45:06 -0300

    L20-Seed-Sample-Products

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,7 @@
 const data = {
   products: [
     {
-      _id: '1',
+      //_id: '1',
       name: 'Nike Slim shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
@@ -14,7 +14,7 @@ const data = {
       description: 'high quality shirt',
     },
     {
-      _id: '2',
+      //_id: '2',
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
@@ -27,7 +27,7 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '3',
+      //_id: '3',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -40,14 +40,14 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '4',
+      //_id: '4',
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-pant',
       category: 'Pants',
       image: '/images/p4.jpeg',
       price: 65,
       countInStock: 5,
-      brand: 'Puma',
+      brand: 'Adidas',
       rating: 4.5,
       numReviews: 10,
       description: 'high quality product',
---
+++ backend/models/productModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const propductSchema = mongoose.Schema(
+  {
+    name: { type: String, require: true, unique: true },
+    slug: { type: String, require: true, unique: true },
+    image: { type: String, require: true },
+    brand: { type: String, require: true },
+    category: { type: String, require: true },
+    description: { type: String, require: true },
+    price: { type: Number, require: true },
+    countInStock: { type: Number, require: true },
+    rating: { type: Number, require: true },
+    numReviews: { type: Number, require: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product', propductSchema);
+
+export default Product;
